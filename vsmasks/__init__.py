41737a8e7de8154abcd948ecf848fc341ba84a65
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-12-13 01:39:48 +0100

    Add masks submodule, `range_mask`

--- vsmasks/__init__.py
+++ vsmasks/__init__.py
@@ -1,5 +1,6 @@
 from .details import *  # noqa: F401, F403
 from .funcs import *  # noqa: F401, F403
+from .masks import *  # noqa: F401, F403
 from .morpho import *  # noqa: F401, F403
 from .types import *  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
