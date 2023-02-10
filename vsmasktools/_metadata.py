6c3d2bce90738ead49a8f9f086b0fa842361f7e4
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-02-10 12:19:03 +0100

    Bump major version

--- vsmasktools/_metadata.py
+++ vsmasktools/_metadata.py
@@ -1,6 +1,6 @@
 """Various masking tools for VapourSynth"""
 
-__version__ = '0.8.3'
+__version__ = '0.9.0'
 
 __author_name__, __author_email__ = 'Irrational Encoding Wizardry', 'wizards@encode.moe'
 __maintainer_name__, __maintainer_email__ = __author_name__, __author_email__
