c9d4ff3651287fb25173620a9196193e95f56ed8
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2024-10-11 21:40:06 +0200

    `__init__`: top level noqa

--- vsmasktools/edge/__init__.py
+++ vsmasktools/edge/__init__.py
@@ -1,5 +1,7 @@
-from ._1d import *  # noqa: F401, F403
-from ._2x2 import *  # noqa: F401, F403
-from ._3x3 import *  # noqa: F401, F403
-from ._5x5 import *  # noqa: F401, F403
-from ._abstract import *  # noqa: F401, F403
+# ruff: noqa: F401, F403
+
+from ._1d import *
+from ._2x2 import *
+from ._3x3 import *
+from ._5x5 import *
+from ._abstract import *
